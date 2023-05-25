a4a7d65f0e6899c2b64fb35bfaa923c11173d4f5
dayane.hernandez <dayane.hernandez@scitum.com.mx>
2023-05-24 19:37:37 -0600

    invent

--- app.py
+++ app.py
@@ -76,6 +76,7 @@ def login():
         cursor = mysql.connection.cursor()
         cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
         user_data = cursor.fetchone()
+        print(user_data)
         if user_data is None:
             flash('El usuario no existe.', 'error')
             return redirect(url_for('login'))
