3ba575816d5779caa45e175aa5067291a7ebb398
dayane.hernandez <dayane.hernandez@scitum.com.mx>
2023-05-24 19:47:37 -0600

    invent

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='localhost',port=80,debug=True)
+    app.run(host='10.0.100.122',port=80,debug=True)
