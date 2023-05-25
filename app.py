dbebce49f47ce5a34a1ca356ba7c3ca3b82950eb
dayane.hernandez <dayane.hernandez@scitum.com.mx>
2023-05-24 19:45:38 -0600

    invent

--- app.py
+++ app.py
@@ -1,77 +1,27 @@
-from flask import Flask, render_template, redirect, url_for, request, flash, session, abort
-from flask_login import LoginManager, login_user, current_user, login_required, logout_user, UserMixin
+from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_mysqldb import MySQL
-from werkzeug.security import check_password_hash
 from datetime import datetime, timedelta
-
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecretkey'
-app.config['MYSQL_HOST'] = 'localhost'
+app.secret_key = 'mysecretkey'
+app.config['MYSQL_HOST'] = 'ventas_flask'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '39023902'
-app.config['MYSQL_DB'] = 'ventas_flask'
+app.config['MYSQL_DB'] = 'mydatabase'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
-app.config['SESSION_COOKIE_SECURE'] = True
 
 mysql = MySQL(app)
 
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-class User(UserMixin):
-    def __init__(self, id, username, password, role, active=True):
-        self.id = id
-        self.username = username
-        self.password = password
-        self.role = role
-        self.is_active = active
-
-    def get_id(self):
-        return str(self.id)
-
-    def is_active(self):
-        return self.active
-
-    @staticmethod
-    def get(user_id):
-        cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM users WHERE id = %s', (user_id,))
-        user_data = cursor.fetchone()
-        if user_data is None:
-            return None
-        user = User(*user_data)
-        return user
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.get(user_id)
-
-@app.before_request
-def before_request():
-    session.permanent = True
-    app.permanent_session_lifetime = timedelta(minutes=30)
-    session.modified = True
-    if current_user.is_authenticated:
-        last_active = session.get('last_active')
-        if last_active is not None and (datetime.utcnow() - last_active) > timedelta(minutes=30):
-            session.clear()
-            return redirect(url_for('login'))
-        session['last_active'] = datetime.utcnow()
-
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-from flask import flash
+    if 'username' in session:
+        return render_template('home.html', username=session['username'])
+    return redirect(url_for('login'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('inventario'))
-
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -81,36 +31,19 @@ def login():
         if user_data is None:
             flash('El usuario no existe.', 'error')
             return redirect(url_for('login'))
-        user = User(*user_data)
-        if not check_password_hash(user.password, password):
+        _, _, hashed_password = user_data
+        if not check_password_hash(hashed_password, password):
             flash('La contraseña es incorrecta.', 'error')
             return redirect(url_for('login'))
-        login_user(user)
-        return redirect(url_for('inventario'))
-
+        session['username'] = username
+        session.permanent = True
+        return redirect(url_for('home'))
     return render_template('login.html')
 
-
 @app.route('/logout')
-@login_required
 def logout():
-    logout_user()
-    session.clear()
-    return redirect(url_for('home'))
-
-@app.route('/admin')
-@login_required
-def admin():
-    if current_user.role != 'admin':
-        abort(403)
-    return render_template('admin.html')
-
-@app.route('/inventario')
-@login_required
-def inventario():
-    if current_user.role not in ['admin', 'user']:
-        abort(403)
-    return
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='10.0.100.122',port=80,debug=False)
+    app.run(debug=True)
